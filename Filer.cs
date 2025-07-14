d0376b53417c2f319003ede8e43fd424310b56f4
fzf003 <280086260@qq.com>
2025-07-14 19:36:38 +0800

    update

--- Filer.cs
+++ Filer.cs
@@ -4,7 +4,7 @@ using Microsoft.SemanticKernel;
 
 public sealed class PromptFilter : IPromptRenderFilter
 {
-     
+
 
     public async Task OnPromptRenderAsync(PromptRenderContext context, Func<PromptRenderContext, Task> next)
     {
@@ -12,10 +12,40 @@ public sealed class PromptFilter : IPromptRenderFilter
         Console.WriteLine("================修改提示语开始===================================");
         Console.WriteLine($"Filter:{context.Function.Name}");
         Console.WriteLine(JsonSerializer.Serialize(context.Arguments));
-       // await context.Kernel.InvokePromptAsync("你是一个精通北京文化人,介绍自己");
+        Console.WriteLine($"是否流:{context.IsStreaming}");
+        // await context.Kernel.InvokePromptAsync("你是一个精通北京文化人,介绍自己");
+        // context.Arguments.Add("prompt", "你是一个精通北京文化人,介绍自己");
+
+       KernelFunction getWeatherForCity = context.Kernel.Plugins.GetFunction("ProductSkill", "GetDateTime");
+
+        var currdatetime = await getWeatherForCity.InvokeAsync<string>(context.Kernel, context.Arguments);
+
         await next(context);
 
+
+        //context.Arguments.Add("currdatetime", currdatetime);
+      
+       // KernelArguments arguments = new() { { "currdatetime", currdatetime } };
+
+        context.RenderedPrompt = $"千问,当前时间:{currdatetime} 夏威夷有什么?";
+
         Console.WriteLine("最终:" + context.RenderedPrompt);
-        Console.WriteLine("===============================================================");
+
+
+
+        Console.WriteLine("================修改提示语结束===================================");
+    }
+}
+
+public class AutoFilter : IAutoFunctionInvocationFilter
+{
+    public async Task OnAutoFunctionInvocationAsync(AutoFunctionInvocationContext context, Func<AutoFunctionInvocationContext, Task> next)
+    {
+        Console.WriteLine("================修改自动提示语开始===================================");
+        Console.WriteLine($"Filter:{context.Function.Name}");
+        Console.WriteLine(JsonSerializer.Serialize(context.Arguments));
+        Console.WriteLine($"是否流:{context.IsStreaming}");
+         Console.WriteLine("最终:" + context.ChatMessageContent.Content);
+        Console.WriteLine("================修改自动提示语结束===================================");
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Azure;
+using Json.Schema.Generation.Intents;
 using Microsoft.AspNetCore.Http.Connections;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -9,27 +11,36 @@ using Microsoft.SemanticKernel.TextGeneration;
 using OpenAI;
 using SemanticKernelApp;
 using System.ClientModel;
+using System.Text;
 
 
-var qwenApiKey = "";
-var githubkey = "";
+var qwenApiKey = "<key>";
+var githubkey = "<key>"; // 替换为你的GitHub密钥
 var modelId = "qwen-plus";
 var qwenEndpoint = "https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions";
- 
+
+var qwenkernel = CreateQwenKernel();
+
+var openkernel = CreaeOpenKernel();
 
 
+var qwenagent = CreateQwenAgent(qwenkernel);
 
-var kernel = CreaeKernel();
- 
+var openagent = CreateOpenAIAgent(openkernel);
 
 
- 
+List<ChatCompletionAgent> agents = new List<ChatCompletionAgent>()
+{
+    qwenagent,
+    openagent
+};
 
-var agent = CreateAgent(kernel);
+CancellationTokenSource tokenSource = new CancellationTokenSource();
 
- 
+//tokenSource.CancelAfter(TimeSpan.FromSeconds(40)); // 设置超时时间为30秒
 
-string agentprompt = "你是一位游客";
+
+string agentprompt = string.Empty;
 
 while (true)
 {
@@ -41,70 +52,156 @@ while (true)
         break;
     }
 
-    await InvokeAgentAsync(agentprompt);
+    try
+    {
+        KernelArguments arguments = new() { { "topic", "sea" } };
 
+        var result = await openkernel.InvokePromptAsync(agentprompt,cancellationToken: tokenSource.Token);
+
+        Console.WriteLine($"{AuthorRole.Assistant}: {result.RenderedPrompt}");
+
+        await InvokeAgentAsync(result.RenderedPrompt, cancellationToken: tokenSource.Token);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
 
 }
 
 
+Kernel CreateQwenKernel()
+{
+    var builder = Kernel.CreateBuilder();
+    //builder.Services.AddSingleton<IPromptRenderFilter, PromptFilter>();
+    builder.Services.AddSingleton<IAutoFunctionInvocationFilter, AutoFilter>();
+    builder.Plugins.AddFromType<ProductSkill>("ProductSkill");
+    builder.Services.AddLogging(services => services.AddConsole().SetMinimumLevel(LogLevel.Trace));
+    builder.Services.AddDashScopeChatCompletion(qwenApiKey, modelId);
+    return builder.Build();
+}
 
-Kernel CreaeKernel()
+Kernel CreaeOpenKernel()
 {
     var builder = Kernel.CreateBuilder();
     var openAiOptions = new OpenAIClientOptions()
     {
         Endpoint = new Uri("https://models.inference.ai.azure.com")
     };
-    //钱
+
     var credential = new ApiKeyCredential(githubkey);
     var ghModelsClient = new OpenAIClient(credential, openAiOptions);
-    builder.AddOpenAIChatCompletion("gpt-4.1", ghModelsClient);
+    builder.AddOpenAIChatCompletion("gpt-4o", ghModelsClient);
     builder.Services.AddSingleton<IPromptRenderFilter, PromptFilter>();
     builder.Plugins.AddFromType<ProductSkill>("ProductSkill");
     builder.Services.AddLogging(services => services.AddConsole().SetMinimumLevel(LogLevel.Trace));
+
     return builder.Build();
 }
-ChatCompletionAgent CreateAgent(Kernel kernel)
+
+ChatCompletionAgent CreateQwenAgent(Kernel kernel)
+{
+
+    KernelFunction getWeatherForCity = kernel.Plugins.GetFunction("ProductSkill", "GetDateTime");
+
+    KernelFunction GetProduct = kernel.Plugins.GetFunction("ProductSkill", "QueryProduct");
+
+
+    ChatCompletionAgent agent = new()
+    {
+        Name = "千问",
+        Instructions = "你是一个精通北京文化的人.",
+        Kernel = kernel.Clone(),
+
+        Arguments = new KernelArguments(new PromptExecutionSettings
+        {
+            ExtensionData = new Dictionary<string, object?>
+              {
+                  { "thinking_budget", 4000 }, // 思考预算，单位毫秒
+                  { "enable_search", true }, // 是否启用搜索
+                  { "enable_thinking", true } // 是否启用思考
+              },
+            FunctionChoiceBehavior = FunctionChoiceBehavior.Auto(functions: new[] { getWeatherForCity, GetProduct }, options: new FunctionChoiceBehaviorOptions { AllowStrictSchemaAdherence = true })
+        })
+    };
+
+    return agent;
+
+}
+
+
+ChatCompletionAgent CreateOpenAIAgent(Kernel kernel)
 {
     ChatCompletionAgent agent = new ChatCompletionAgent
     {
-        Name = "Qwen",
-        Instructions = "你是一个精通A股的股神，没有你不知道的投资逻辑，善于推荐投资价值的股票",//"你是一个热点新闻评论者,有犀利的语言风格直戳事件本质",
+        Name = "OpenAI",
+        Instructions = "你是一个精通A股的股神,没有你不知道的投资逻辑,善于推荐投资价值的股票",//"你是一个热点新闻评论者,有犀利的语言风格直戳事件本质",
         Kernel = kernel.Clone(),
-        Arguments= new(new PromptExecutionSettings { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto(options: new FunctionChoiceBehaviorOptions {  AllowStrictSchemaAdherence = true }) })
+        Arguments = new(new PromptExecutionSettings { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto(options: new FunctionChoiceBehaviorOptions { AllowStrictSchemaAdherence = true }) })
     };
 
     return agent;
 }
 
 
-async Task InvokeAgentAsync(string message)
+
+async Task InvokeAgentAsync(string message, bool IsStream = true, CancellationToken cancellationToken = default)
 {
-    /*  KernelFunction GetProduct = kernel.Plugins.GetFunction("ProductSkill", "QueryProduct");
-
-      KernelFunction getCurrentTime = kernel.Plugins.GetFunction("ProductSkill", "GetDateTime");
-
-      var arguments = new KernelArguments
-      {
-          ["productId"] = message
-      };
-
-      var orderitem = await GetProduct.InvokeAsync<string>(kernel, arguments);
-
-      var gettime = await getCurrentTime.InvokeAsync<string>(kernel);
-
-      Console.WriteLine($"查询结果:{gettime}:{orderitem}");
-    */
 
 
-    Console.WriteLine($"{AuthorRole.User}: {message}");
-    var messagePrompt = new ChatMessageContent(AuthorRole.User, message);
-    Console.Write($"{AuthorRole.System}: ");
-    await foreach (var response in agent.InvokeStreamingAsync(messagePrompt))
+    List<string> history = new() { "北京", "千问" };
+
+    ChatCompletionAgent agent = null;
+
+
+
+    if (history.Any(p => message.Contains(p)))
     {
-        if (!string.IsNullOrEmpty(response.Message.Content))
+        Console.WriteLine("切换到千问");
+        agent = agents.Where(p => p.Name == "千问").FirstOrDefault();
+    }
+    else
+    {
+        agent = agents.Where(p => p.Name == "OpenAI").FirstOrDefault();
+    }
+
+
+
+    if (agent is { })
+    {
+
+
+        // message = result?.RenderedPrompt;
+        var messagePrompt = new ChatMessageContent(AuthorRole.User, message);
+        Console.Write($"{AuthorRole.System}: ");
+
+        List<ChatMessageContent> chatMessageContents = new List<ChatMessageContent>()
+           {
+               new ChatMessageContent(AuthorRole.User, message,encoding:Encoding.UTF8),
+               new ChatMessageContent(AuthorRole.System, $"北京时间:{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}",encoding:Encoding.UTF8)
+           };
+
+        if (IsStream)
         {
-            Console.Write(response.Message.Content);
+            await foreach (var response in agent!.InvokeStreamingAsync(chatMessageContents, cancellationToken: cancellationToken))
+            {
+                if (!string.IsNullOrEmpty(response.Message.Content))
+                {
+                    Console.Write(response.Message.Content);
+                }
+            }
+        }
+        else
+        {
+            await agent!.InvokeAsync(chatMessageContents, cancellationToken: tokenSource.Token).ForEachAsync(p =>
+            {
+
+                if (!string.IsNullOrEmpty(p.Message.Content))
+                {
+                    Console.Write(p.Message.Content);
+                }
+            });
+
         }
     }
 
